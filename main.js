14432667989754d0a46dc4e3dc7cb889bb2194cd
Oussama Oulhaj <51234577+ouuoussama@users.noreply.github.com>
2021-10-12 15:09:53 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let closemenu = document.getElementById("closemenu");
+let openmenu = document.getElementById("respmenu");
+openmenu.addEventListener("click", open);
+function open() {
+    document.getElementById("menu").style.visibility = "unset";
+  }
+  closemenu.addEventListener("click", close);
+  function close() {
+    document.getElementById("menu").style.visibility = "hidden";
+  }
+// Create a media condition that targets viewports at least 768px wide
+
+// Check if the media query is true
